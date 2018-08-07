d448ab1b3673e8e30068ce20c0acd145753bd47b
woohee <woohee@osci.kr>
2018-08-07 14:46:06 +0900

    normalize ignoreEmpty default false

--- src/search/normalize.js
+++ src/search/normalize.js
@@ -1,10 +1,14 @@
-export default function normalize(obj) {
+export default function normalize(obj, isEmptyIgnored = false) {
   const keys = Object.keys(obj);
   const l = keys.length;
   for (let i = 0; i < l; i++) {
     const key = keys[i];
     const value = obj[key];
-    if (value === null || value === undefined) {
+    if (
+      value === null ||
+      value === undefined ||
+      (isEmptyIgnored === false && value === "")
+    ) {
       delete obj[key];
     }
   }
