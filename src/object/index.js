2bb281623ae7c4c8f4e8415503c6782e86b4d09d
woohee <woohee@osci.kr>
2019-01-14 04:50:29 +0900

    patch

--- src/object/index.js
+++ src/object/index.js
@@ -2,8 +2,10 @@ export { default as shallowEqual } from "./shallowEqual";
 export { default as is } from "./is";
 export { default as getAssignedByKeys } from "./getAssignedByKeys";
 export { default as getAssignedByExceptions } from "./getAssignedByExceptions";
-export { default as shallowEqualByKeys } from shallowEqualByKeys
-export { default as shallowEqualByExceptions } from "./shallowEqualByExceptions";
+export { default as shallowEqualByKeys } from "./shallowEqualByKeys";
+export {
+  default as shallowEqualByExceptions
+} from "./shallowEqualByExceptions";
 export { default as isValidObject } from "./isValidObject";
 export { default as isFromNative } from "./isFromNative";
 export { default as deepAssign } from "./deepAssign";
